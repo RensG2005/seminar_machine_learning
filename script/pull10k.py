2ae4fe0f0fd4b410ade1dccf8bcae29d626975e2
tonyhurioglu <tony@trhur.com>
2026-03-15 15:00:29 +0100

    Updated pull10k

--- script/pull10k.py
+++ script/pull10k.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+data_dir = BASE_DIR / "data" / "raw 10k"
+data_dir.mkdir(parents=True, exist_ok=True)
 
 url = "https://www.sec.gov/Archives/edgar/data/1318605/000095017022000796/tsla-20211231.htm"
 
@@ -15,3 +20,7 @@ html = response.text
 soup = BeautifulSoup(html, "html.parser")
 text = soup.get_text(" ", strip=True)
 
+file_path = data_dir / "tesla_2021.txt"
+
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write(text)
