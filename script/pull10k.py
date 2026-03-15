4a85a4a2536db05ab8697ff94625449ba57162a8
tonyhurioglu <tony@trhur.com>
2026-03-15 15:12:49 +0100

    Updated pull10k

--- script/pull10k.py
+++ script/pull10k.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
+import re
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 data_dir = BASE_DIR / "data" / "raw 10k"
 data_dir.mkdir(parents=True, exist_ok=True)
+file_path = data_dir / "tesla_2021.txt"
 
 url = "https://www.sec.gov/Archives/edgar/data/1318605/000095017022000796/tsla-20211231.htm"
 
@@ -15,12 +17,19 @@ headers = {
 }
 
 response = requests.get(url, headers=headers)
+response.raise_for_status()
+
 html = response.text
 
 soup = BeautifulSoup(html, "html.parser")
-text = soup.get_text(" ", strip=True)
 
-file_path = data_dir / "tesla_2021.txt"
+for tag in soup(["script", "style"]):
+    tag.decompose()
+
+text = soup.get_text(" ", strip=True)
+text = re.sub(r"\s+", " ", text)
 
 with open(file_path, "w", encoding="utf-8") as f:
     f.write(text)
+
+print("Saved 10-K to:", file_path)
